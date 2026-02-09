fb83a72304014af04388d33e8de2e4ae3d0a828f
Ashraf khaled <ashraf.khaled.w@gmail.com>
2026-02-09 17:22:19 +0200

    change the the error catch in the eventqueue file to throw the erro the call function

--- Modules/eventQueue.js
+++ Modules/eventQueue.js
@@ -8,7 +8,7 @@ const queueUserSafly = async (data) => {
   try {
     await data();
   } catch (err) {
-    console.log(err);
+    throw err;
   } finally {
     isWritingUser = false;
   }
@@ -24,7 +24,7 @@ const queuePostSafly = async (data) => {
   try {
     await data();
   } catch (err) {
-    console.log(err);
+    throw err;
   } finally {
     isWritingPost = false;
   }
